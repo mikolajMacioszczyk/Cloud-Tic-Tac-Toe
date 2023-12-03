1977ab8139132e484910221ac7432f594046347b
Mikołaj <macioszczyk.mikolaj@gmail.com>
2023-12-03 17:39:42 +0100

    regenerate migration

--- BE/CloudTicTacToeBackend/CloudTicTacToe.Infrastructure/Migrations/20231203163911_AddPointsColumnToPlayerTable.Designer.cs
+++ BE/CloudTicTacToeBackend/CloudTicTacToe.Infrastructure/Migrations/20231203163911_AddPointsColumnToPlayerTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CloudTicTacToe.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CloudTicTacToe.Infrastructure.Migrations
 {
     [DbContext(typeof(TicTacToeContext))]
-    partial class TicTacToeContextModelSnapshot : ModelSnapshot
+    [Migration("20231203163911_AddPointsColumnToPlayerTable")]
+    partial class AddPointsColumnToPlayerTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BE/CloudTicTacToeBackend/CloudTicTacToe.Infrastructure/Migrations/20231203163911_AddPointsColumnToPlayerTable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudTicTacToe.Infrastructure.Migrations
+{
+    public partial class AddPointsColumnToPlayerTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GameBoards_Players_PlayerOId",
+                table: "GameBoards");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Points",
+                table: "Players",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PlayerOId",
+                table: "GameBoards",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GameBoards_Players_PlayerOId",
+                table: "GameBoards",
+                column: "PlayerOId",
+                principalTable: "Players",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GameBoards_Players_PlayerOId",
+                table: "GameBoards");
+
+            migrationBuilder.DropColumn(
+                name: "Points",
+                table: "Players");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PlayerOId",
+                table: "GameBoards",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GameBoards_Players_PlayerOId",
+                table: "GameBoards",
+                column: "PlayerOId",
+                principalTable: "Players",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
