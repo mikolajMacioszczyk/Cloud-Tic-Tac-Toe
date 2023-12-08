3539e85ead9030fdff798c7b6f236f5aa25788de
Mikołaj <macioszczyk.mikolaj@gmail.com>
2023-12-08 08:26:02 +0100

    added environments

--- FE/CloudTicTacToe/src/app/services/game.api.service.ts
+++ FE/CloudTicTacToe/src/app/services/game.api.service.ts
@@ -2,29 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GameBoard } from '../models/game-board';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameApiService {
-  // TODO: Get api url from variable
-  private apiUrl = 'http://localhost:8080/api/Game';
+  private baseUrl = environment.apiUrl + '/Game';
 
   constructor(private http: HttpClient) {}
 
   getById(gameId: string): Observable<GameBoard> {
-    return this.http.get<GameBoard>(this.apiUrl + '/' + gameId);
+    return this.http.get<GameBoard>(this.baseUrl + '/' + gameId);
   }
 
   initializeWithComputer(body: { playerId: string }): Observable<GameBoard> {
-    return this.http.post<GameBoard>(this.apiUrl + '', body);
+    return this.http.post<GameBoard>(this.baseUrl + '', body);
   }
 
   initializeOrJoinOnlineGame(body: { playerId: string }): Observable<GameBoard> {
-    return this.http.post<GameBoard>(this.apiUrl + '/online', body);
+    return this.http.post<GameBoard>(this.baseUrl + '/online', body);
   }
 
   playTurn(body: { id: string, playerId: string, rowNumber: number, colNumber: number }): Observable<GameBoard> {
-    return this.http.put<GameBoard>(this.apiUrl + '/' + body.id + '/actions/play', body);
+    return this.http.put<GameBoard>(this.baseUrl + '/' + body.id + '/actions/play', body);
   }
 }
--- FE/CloudTicTacToe/src/app/services/player.api.service.ts
+++ FE/CloudTicTacToe/src/app/services/player.api.service.ts
@@ -2,21 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Player } from '../models/player';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlayerApiService {
-  // TODO: Get api url from variable
-  private apiUrl = 'http://localhost:8080/api/Player';
+  private baseUrl = environment.apiUrl + '/Player';
 
   constructor(private http: HttpClient) {}
 
   getAllPlayers(): Observable<Player[]> {
-    return this.http.get<Player[]>(this.apiUrl);
+    return this.http.get<Player[]>(this.baseUrl);
   }
 
   registerPlayer(body: {name: string}): Observable<Player> {
-    return this.http.post<Player>(this.apiUrl + '', body);
+    return this.http.post<Player>(this.baseUrl + '', body);
   }
 }
